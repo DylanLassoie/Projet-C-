c1d8b9c96074d8e8f9be73c93eeea9cce0d3bed6
ddeblander <david.deblander@condorcet.be>
2023-01-04 16:47:40 +0100

    -Creation basic GUI  to show data and Login

--- Projet C/MainWindow.xaml.cs
+++ Projet C/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Projet_C.Management;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,50 @@ namespace Projet_C
         public MainWindow()
         {
             InitializeComponent();
+            menu_selection.SelectedIndex = 0;
+        }
+
+        private void Selector_OnSelected(object sender, RoutedEventArgs e)
+        {
+            var listView = (ListView)sender;
+            switch (listView.SelectedIndex)
+            {
+                case 0:
+                    grid_user.Visibility = Visibility.Visible;
+                    grid_video_game.Visibility = Visibility.Collapsed;
+                    grid_user_loan.Visibility = Visibility.Collapsed;
+                    ShowUserList();
+                    break;
+                case 1:
+                    grid_user.Visibility = Visibility.Collapsed;
+                    grid_video_game.Visibility = Visibility.Visible;
+                    grid_user_loan.Visibility = Visibility.Collapsed;
+                    break;
+                case 2:
+                    grid_user.Visibility = Visibility.Collapsed;
+                    grid_video_game.Visibility = Visibility.Collapsed;
+                    grid_user_loan.Visibility = Visibility.Visible;
+                    break;
+            }
+        }
+
+        private void ShowUserList()
+        {
+            dg_user.ItemsSource = Dao.GetUsers();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            string pwd = Dao.GetPwdByUserName(login_user.Text);
+            if (login_pwd.Password != pwd)
+            {
+                login_error_text.Visibility = Visibility.Visible;
+                login_pwd.Password = string.Empty;
+                return;
+            }
+
+            Login_ui.Visibility = Visibility.Collapsed;
+
         }
     }
 }
