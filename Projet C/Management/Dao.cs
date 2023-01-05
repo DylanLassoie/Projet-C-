816b12691c2a8a9b16e7ba1dbf444ef0837a61a6
ddeblander <david.deblander@condorcet.be>
2023-01-05 17:27:21 +0100

    -database Connection created need to be link with DAO.cs

--- Projet C/Management/Dao.cs
+++ Projet C/Management/Dao.cs
@@ -4,11 +4,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
+
 
 namespace Projet_C.Management
 {
     public class Dao
     {
+
         private static List<Player> users = new List<Player> { new Player("Pseudo", new DateTime(22 / 03 / 2022), DateTime.Now) { Username = "UserName", Password = "Password", Pseudo = "Pseudo" } };
         public static List<Player> GetUsers()
         {
